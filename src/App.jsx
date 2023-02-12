424e74ef9f7ce5573cf692873fece01e48518c71
anikettsatpute <cs21btech11056@iith.ac.in>
2023-02-13 00:04:56 +0530

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
+<<<<<<< Updated upstream
 import styles from './App.module.css'
 import {Home} from './pages/Home/Home.jsx'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 
+=======
+import "./App.css";
+import Login from "./components/Login/Login";
+import Register from "./components/register/Register";
+>>>>>>> Stashed changes
 function App() {
-
   return (
+<<<<<<< Updated upstream
     <div className="App">
       <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home/>}/>
       </Routes>
       </BrowserRouter>
+=======
+    <div>
+      <Register />
+>>>>>>> Stashed changes
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import styles from './Navbar.module.css'
-import {MagnifyingGlass} from 'phosphor-react'
-
+import React from "react";
+import styles from "./Navbar.module.css";
+import { MagnifyingGlass } from "phosphor-react";
 
 function Navbar() {
   return (
-    
     <div>
+<<<<<<< Updated upstream
  <div className={styles.Navbar}>
     <div className={styles.name}>
       <img src="se-icon.png" alt="no-image" className={styles.icon}/>
@@ -24,11 +23,24 @@ function Navbar() {
     <button type="button" className={"btn btn-primary "+styles.loginbtn}>SignUp</button>
     <button type="button" className={"btn btn-primary "+styles.loginbtn}>Login</button>
     </div>
+=======
+      <div className={styles.Navbar}>
+        <div className={styles.name}>
+          <img src="icon.png" alt="no-image" />
+          Text 1
+        </div>
+        <div className={styles.searchwrapper}>
+          <div className={styles.searchicon}>
+            <MagnifyingGlass size={24} color="#812222" weight="regular" />
+          </div>
+          <input type="text" className={styles.search} placeholder="Search" />
+        </div>
+        <div className={styles.signin}>Text3</div>
+>>>>>>> Stashed changes
       </div>
+      <div className={styles.text}></div>
     </div>
-
-   
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
