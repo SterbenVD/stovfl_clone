39b97411a69927a46ea7dd1ef9c1a9f8c3cdf561
Vishal Devadiga <vishaldevadiga003@gmail.com>
2023-03-05 16:51:58 +0530

    Fix route

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -11,7 +11,7 @@ const router = express.Router();
 router.get("/auth", authC.authUser); // Works
 router.post("/user", authC.createUser);
 router.get("/user/:user_name", userC.getUserByName); // Works
-router.get("/usersearch/:id", userC.getFuzzyUser); // Works
+router.get("/usersearch", userC.getFuzzyUser); // Works
 
 router.get("/post/:id", postC.getPostById); // Works
 router.get("/post/user/:id/:post_type/:sort/:order/", postC.PostsByUser); // Works
