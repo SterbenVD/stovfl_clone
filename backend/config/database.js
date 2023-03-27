367960dfc5aa08adfbe7b7c4c157461ec029aa80
anikettsatpute <cs21btech11056@iith.ac.in>
2023-03-27 11:06:09 +0530

    ats

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,13 +1,9 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("cqadb", "postgres", "123", {
+const db = new Sequelize("cqadb", "postgres", "2245", {
   host: "localhost",
   dialect: "postgres",
   logging: console.log,
 });
-
-// Syncing all models
-// await sequelize.sync();
-// console.log("All models were synchronized successfully.");
 
 export default db;
